00b7b8704a9edd5de02f1afe7e411233ae2b944a
techid2000 <techid2000@gmail.com>
2019-12-25 00:11:53 +0700

    Updates

---
+++ precache-manifest.ca7ebb367e36a62001453737c0e888ba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "630f76e1e4dc539a401ebf11a36261d2",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "d6236e96287c6528ac70",
+    "url": "/monsters-rolodex/static/css/main.75cef6f1.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d6236e96287c6528ac70",
+    "url": "/monsters-rolodex/static/js/main.ce69a03e.chunk.js"
+  },
+  {
+    "revision": "517fd9319d43d3a57612",
+    "url": "/monsters-rolodex/static/js/runtime-main.ae75c310.js"
+  }
+]);
